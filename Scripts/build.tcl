8f8aa0bac7304b974041f3cb663e43e102b35dcd
ashwin <ashwinjohn28@gmail.com>
2024-05-28 09:23:02 -0700

    added subfolders to build

--- Scripts/build.tcl
+++ Scripts/build.tcl
@@ -18,30 +18,30 @@ read_xdc C:/Users/ashwi/Documents/Vivado_Projects/RISCV-1/Constraints/Arty-Z7-20
 
 #Run Synthesis
 synth_design -top top -part $partNum
-write_checkpoint -force $outputDir/post_synth.dcp
-report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
-report_utilization -file $outputDir/post_synth_util.rpt
+write_checkpoint -force $outputDir/Synthesis.runs/post_synth.dcp
+report_timing_summary -file $outputDir/Synthesis.runs/post_synth_timing_summary.rpt
+report_utilization -file $outputDir/Synthesis.runs/post_synth_util.rpt
 
 #run optimization
 opt_design
 place_design
-report_clock_utilization -file $outputDir/clock_util.rpt
+report_clock_utilization -file $outputDir/Implementation.runs/clock_util.rpt
 
 #get timing violations and run optimizations if needed
 if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
  puts "Found setup timing violations => running physical optimization"
  phys_opt_design
 }
-write_checkpoint -force $outputDir/post_place.dcp
-report_utilization -file $outputDir/post_place_util.rpt
-report_timing_summary -file $outputDir/post_place_timing_summary.rpt
+write_checkpoint -force $outputDir/Implementation.runs/post_place.dcp
+report_utilization -file $outputDir/Implementation.runs//post_place_util.rpt
+report_timing_summary -file $outputDir/Implementation.runs/post_place_timing_summary.rpt
 
 #Route design and generate bitstream
 route_design -directive Explore
-write_checkpoint -force $outputDir/post_route.dcp
-report_route_status -file $outputDir/post_route_status.rpt
-report_timing_summary -file $outputDir/post_route_timing_summary.rpt
-report_power -file $outputDir/post_route_power.rpt
-report_drc -file $outputDir/post_imp_drc.rpt
-write_verilog -force $outputDir/cpu_impl_netlist.v -mode timesim -sdf_anno true
+write_checkpoint -force $outputDir/Implementation.runs/post_route.dcp
+report_route_status -file $outputDir/Implementation.runs//post_route_status.rpt
+report_timing_summary -file $outputDir/Implementation.runs/post_route_timing_summary.rpt
+report_power -file $outputDir/Implementation.runs/post_route_power.rpt
+report_drc -file $outputDir/Implementation.runs/post_imp_drc.rpt
+write_verilog -force $outputDir/Implementation.runs/impl_netlist.v -mode timesim -sdf_anno true
 write_bitstream -force $outputDir/nameOfBitstream.bit
